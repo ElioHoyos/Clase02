fb80e5dea698247dd7b5eccdf36b2c8418a91ca1
Elio Hoyos <elio18david@gmail.com>
2024-08-23 12:04:49 -0500

    lottie json animation

---
+++ app/src/main/java/com/example/clase2/PantallaCarga.java
@@ -0,0 +1,20 @@
+package com.example.clase2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+
+public class PantallaCarga extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla_carga);
+
+        int Tiempo = 3000;
+
+        
+
+    }
+}
